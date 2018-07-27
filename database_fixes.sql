e01e9f95eb369193897a858d18ec539bf0d46e99
Dan Clark <wpu.clark@gmail.com>
2018-07-27 17:01:23 -0400

    Alter the stored procedure and view so that the tests pass

---
+++ database_fixes.sql
@@ -0,0 +1,17 @@
+
+ALTER PROCEDURE get_project_costs (
+    @project_id INT
+)
+AS (
+    SELECT SUM(ps.quantity * ps.unit_cost)
+    FROM projects p
+    JOIN project_supplies ps on p.id = ps.project_id
+)
+
+ALTER VIEW projects_and_supplies
+AS
+    SELECT p.name project, ps.name part, ps.quantity
+    FROM projects p
+    JOIN project_supplies ps on p.id = ps.project_id
+
+
