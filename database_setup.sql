4d49af360109146ca938bddb7bdceb91b723ba0a
Dan Clark <wpu.clark@gmail.com>
2018-07-27 16:25:57 -0400

    Add database schema for stored procedure and view

--- database_setup.sql
+++ database_setup.sql
@@ -19,4 +19,19 @@ CREATE TABLE project_steps (
     step_number VARCHAR(20) NOT NULL
 )
 
+CREATE PROCEDURE get_project_costs (
+    @project_id INT
+)
+AS (
+    SELECT SUM(ps.quantity + ps.unit_cost)
+    FROM projects p
+    JOIN project_supplies ps on p.id = ps.project_id
+)
+
+CREATE VIEW projects_and_supplies
+AS
+    SELECT p.name project, ps.name part, ps.quantity
+    FROM projects p
+    JOIN project_supplies ps on p.id = p.id
+
 
