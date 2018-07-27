bba7704b245ad768730e7708425b17c328dafa19
Dan Clark <wpu.clark@gmail.com>
2018-07-27 15:47:10 -0400

    Change tests to reflect new project tables

--- test_database.py
+++ test_database.py
@@ -1,49 +1,34 @@
-import pytest
-import pyodbc
 
-@pytest.fixture(scope='module')
-def password():
-    with open('pass.word') as f:
-        pw = f.readline().strip()
-    yield pw
-
-@pytest.fixture(scope='module')
-def cnxn(password):
-    cnxn = pyodbc.connect('DSN=bruisedthumb;UID=bruised;PWD={}'.format(password))
-    yield cnxn
-    cnxn.close()
-
-@pytest.fixture
-def cursor(cnxn):
-    cursor = cnxn.cursor()
-    yield cursor
-    cnxn.rollback()
-
-def test_delivered_product_table_row_count(cursor):
-    cursor.execute('select name from saleslt.product')
+def test_project_table_exists(cursor, first_project):
+    cursor.execute('select id from projects')
     rs = cursor.fetchall()
-    assert len(rs) == 295
+    assert len(rs) == 1 
 
-def test_delivered_customer_table_row_count(cursor):
-    cursor.execute('select firstname from saleslt.customer')
+def test_project_supplies_table_exists(cursor):
+    cursor.execute('select id from project_supplies')
     rs = cursor.fetchall()
-    assert len(rs) == 847 
+    assert len(rs) == 0 
 
-def test_update_customers(cursor):
-    lastname = cursor.execute('select lastname from saleslt.customer where customerid = 1').fetchval()
-    print(lastname)
-    assert lastname == 'McGee'
-    cursor.execute("update saleslt.customer set lastname = 'Gibbs' where customerid = 1")
-    lastname = cursor.execute('select lastname from saleslt.customer where customerid = 1').fetchval()
-    print(lastname)
-    assert lastname == 'Gibbs'
+def test_project_steps_table_exists(cursor):
+    cursor.execute('select id from project_steps')
+    rs = cursor.fetchall()
+    assert len(rs) == 0 
 
-
-def test_update_customers_again(cursor):
-    lastname = cursor.execute('select lastname from saleslt.customer where customerid = 1').fetchval()
-    print(lastname)
-    assert lastname == 'McGee'
-    cursor.execute("update saleslt.customer set lastname = 'Gibbs' where customerid = 1")
-    lastname = cursor.execute('select lastname from saleslt.customer where customerid = 1').fetchval()
-    print(lastname)
-    assert lastname == 'Gibbs'
+# def test_update_customers(cursor):
+#     lastname = cursor.execute('').fetchval()
+#     print(lastname)
+#     assert lastname == 'McGee'
+#     cursor.execute("update saleslt.customer set lastname = 'Gibbs' where customerid = 1")
+#     lastname = cursor.execute('select lastname from saleslt.customer where customerid = 1').fetchval()
+#     print(lastname)
+#     assert lastname == 'Gibbs'
+# 
+# 
+# def test_update_customers_again(cursor):
+#     lastname = cursor.execute('select lastname from saleslt.customer where customerid = 1').fetchval()
+#     print(lastname)
+#     assert lastname == 'McGee'
+#     cursor.execute("update saleslt.customer set lastname = 'Gibbs' where customerid = 1")
+#     lastname = cursor.execute('select lastname from saleslt.customer where customerid = 1').fetchval()
+#     print(lastname)
+#     assert lastname == 'Gibbs'
